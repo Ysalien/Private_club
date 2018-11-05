ded5498c6ffecd9f3a81542c135ca5777b38bb91
Ysalien <enilasy.m@gmail.com>
2018-11-06 00:44:19 +0100

    Edit modifié

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,10 +24,6 @@ class SessionsController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
     @user = User.find(params[:id])
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_id)
